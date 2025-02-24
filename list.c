711c36742aa6bba95a0e6e047349832f6609788d
Angelos Ballis <narbanite@gmail.com>
2025-02-24 21:34:52 +0200

    created a header file for the token list

---
+++ list.c
@@ -0,0 +1,85 @@
+#include "list.h"
+
+alpha_token_t* createTokenNode(unsigned int line, unsigned int num_token, char* zoumi, unsigned int category){
+    alpha_token_t* neoToken = (alpha_token_t*)malloc(sizeof(alpha_token_t));
+    if (!neoToken) {
+        printf("memory alloc problem!\n");
+        return NULL;
+    }
+
+    neoToken->line = line;
+    neoToken->num_token = num_token;
+    neoToken->category = category;
+    neoToken->zoumi = strdup(zoumi);
+    neoToken->next = NULL;
+    
+    return neoToken;
+}
+
+void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token, char* zoumi, unsigned int category){
+    alpha_token_t* neoToken = createTokenNode(line, num_token, zoumi, category);
+    if (!neoToken) return;
+
+    if (*root == NULL) {
+        *root = neoToken;
+        return;
+    }
+
+    alpha_token_t* temp = *root;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    temp->next = neoToken;
+}
+
+void deleteToken(alpha_token_t** root, unsigned int num_token){
+    if (*root == NULL) return;
+
+    alpha_token_t* temp = *root;
+    alpha_token_t* prev = NULL;
+
+    //if the node is the head of the list
+    if (temp && temp->num_token == num_token) {
+        *root = temp->next;
+        free(temp->zoumi); 
+        free(temp);
+        return;
+    }
+
+    //search for the token
+    while (temp && temp->num_token != num_token) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (!temp) return; //not found
+
+    prev->next = temp->next;
+    free(temp->zoumi);
+    free(temp);
+}
+
+alpha_token_t* searchToken(alpha_token_t* root, unsigned int num_token){
+    alpha_token_t* temp = root;
+    while (temp) {
+        if (temp->num_token == num_token) {
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+void printTokens(alpha_token_t* root){
+    //AUTH EDW H SUNARTHSH PREPEI NA EXEI TO FORMA TOU FAQ!! SOS
+}
+
+void freeTokenList(alpha_token_t** root){
+    alpha_token_t* temp;
+    while (*root) {
+        temp = *root;
+        *root = (*root)->next;
+        free(temp->zoumi); 
+        free(temp);
+    }
+}
---
+++ list.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct alpha_token_t {
+    unsigned int line;
+    unsigned int num_token;
+
+    char* zoumi;
+    unsigned int category;
+    
+    struct alpha_token_t* next; 
+}alpha_token_t;
+
+
+alpha_token_t* createTokenNode(unsigned int line, unsigned int num_token, char* zoumi, unsigned int category);
+void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token, char* zoumi, unsigned int category);
+void deleteToken(alpha_token_t** root, unsigned int num_token);
+alpha_token_t* searchToken(alpha_token_t* root, unsigned int num_token);
+void printTokens(alpha_token_t* root);
+void freeTokenList(alpha_token_t** root);
+
+#endif 
