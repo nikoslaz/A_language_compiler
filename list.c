e20ba2ebda9b02fe2952888946cb1f0947d01229
ElsieUwU <mixaliskritsotakis@gmail.com>
2025-02-25 16:34:35 +0200

    Actually INSANE

--- list.c
+++ list.c
@@ -1,25 +1,90 @@
+/* list.c */
+
 #include "list.h"
 
 alpha_token_t* root = NULL;
 int tokenCounter = 1;
 
 char* CAT[] = {
-    "todo",
-    "KEYWORD",
-    "CONST_INT",
-    "STRING",
-    "IF",
-    "ELSE",
-    "WHILE",
-    "enumerated",
-    "char*",
-    "INTEGER",
-    "ID"
+    "ERR",         /* 0 */
+    "KEYWORD",     /* 1 */
+    "OPERATOR",    /* 2 */
+    "CONST_INT",   /* 3 */
+    "CONST_REAL",  /* 4 */
+    "STRING",      /* 5 */
+    "PUNCTUATION", /* 6 */
+    "ID",          /* 7 */
+    "COMMENT",     /* 8 */
 };
 
-alpha_token_t* createTokenNode (unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, char* subcategory, unsigned int superclass){
+char* NAM[] = {
+    "ERR",      /* 0 */
+    "IF",       /* 1 */
+    "ELSE",     /* 2 */
+    "WHILE",    /* 3 */
+    "FOR",      /* 4 */
+    "FUNCTION", /* 5 */
+    "RETURN",   /* 6 */
+    "BREAK",    /* 7 */
+    "CONTINUE", /* 8 */
+    "AND",      /* 9 */
+    "OR",       /* 10 */
+    "NOT",      /* 11 */
+    "LOCAL",    /* 12 */
+    "TRUE",     /* 13 */
+    "FALSE",    /* 14 */
+    "NIL",      /* 15 */
+
+    "EQUALS",        /* 16 */
+    "PLUS",          /* 17 */
+    "MINUS",         /* 18 */
+    "MULT",          /* 19 */
+    "DIV",           /* 20 */
+    "MOD",           /* 21 */
+    "EQUALS_EQUALS", /* 22 */
+    "NOT_EQUALS",    /* 23 */
+    "PLUS_PLUS",     /* 24 */
+    "MINUS_MINUS",   /* 25 */
+    "GREATER",       /* 26 */
+    "LESS",          /* 27 */
+    "GREATER_EQUAL", /* 28 */
+    "LESS_EQUAL",    /* 29 */
+
+    "LEFT_BRACE",        /* 30 */
+    "RIGHT_BRACE",       /* 31 */
+    "LEFT_BRACKET",      /* 32 */
+    "RIGHT_BRACKET",     /* 33 */
+    "LEFT_PARENTHESIS",  /* 34 */
+    "RIGHT_PARENTHESIS", /* 35 */
+    "SEMICOLON",         /* 36 */
+    "COMMA",             /* 37 */
+    "COLON",             /* 38 */
+    "COLON_COLON",       /* 39 */
+    "PERIOD",            /* 40 */
+    "PERIOD_PERIOD",     /* 41 */
+
+    "LINE_COMMENT",      /* 42 */
+    "PYTHON_COMMENT",    /* 43 */
+    "MULTILINE_COMMENT", /* 44 */
+
+    /* To print zoumi */
+    "INTEGER", /* 45 */
+    "REAL",    /* 46 */
+    "STRING"   /* 47 */
+    "ID",      /* 48 */
+};
+
+char* SUP[] = {
+    "ERR",        /* 0 */
+    "char*",      /* 1 */
+    "enumerated", /* 2 */
+    "integer",    /* 3 */
+    "real"        /* 4 */
+};
+
+alpha_token_t* createTokenNode(unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, unsigned int name, unsigned int superclass) {
     alpha_token_t* neoToken = (alpha_token_t*)malloc(sizeof(alpha_token_t));
-    if (!neoToken) {
+    if(!neoToken) {
         printf("memory alloc problem!\n");
         return NULL;
     }
@@ -28,25 +93,27 @@ alpha_token_t* createTokenNode (unsigned int line, unsigned int num_token, char*
     neoToken->num_token = num_token;
     neoToken->zoumi = strdup(zoumi);
     neoToken->category = category;
-    neoToken->subcategory = strdup(subcategory);
+    neoToken->name = name;
     neoToken->superclass = superclass;
     neoToken->next = NULL;
 
     return neoToken;
 }
 
-void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, char* subcategory, unsigned int superclass){
-    /*TO DO: na allaksei to unsigned int se CAT (to enum pou ftiaksame)*/
-    alpha_token_t* neoToken = createTokenNode(line, num_token, zoumi, category, subcategory, superclass);
-    if (!neoToken) return;
+void destroyToken(alpha_token_t* temp) {
+    free(temp->zoumi);
+    free(temp);
+}
 
-    if (*root == NULL) {
+void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, unsigned int name, unsigned int superclass) {
+    alpha_token_t* neoToken = createTokenNode(line, num_token, zoumi, category, name, superclass);
+    if(!neoToken) return;
+    if(*root == NULL) {
         *root = neoToken;
         return;
     }
-
     alpha_token_t* temp = *root;
-    while (temp->next) {
+    while(temp->next) {
         temp = temp->next;
     }
     temp->next = neoToken;
@@ -54,36 +121,30 @@ void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token
 
 void deleteToken(alpha_token_t** root, unsigned int num_token){
     if (*root == NULL) return;
-
     alpha_token_t* temp = *root;
     alpha_token_t* prev = NULL;
-
     //if the node is the head of the list
-    if (temp && temp->num_token == num_token) {
+    if(temp && temp->num_token == num_token) {
         *root = temp->next;
-        free(temp->zoumi); 
-        free(temp->subcategory);
-        free(temp);
+        destroyToken(temp);
         return;
     }
-
     //search for the token
-    while (temp && temp->num_token != num_token) {
+    while(temp && temp->num_token != num_token) {
         prev = temp;
         temp = temp->next;
     }
-
-    if (!temp) return; //not found
-
+    //not found
+    if(!temp) { return; }
+    // remove from list
     prev->next = temp->next;
-    free(temp->zoumi);
-    free(temp);
+    destroyToken(temp);
 }
 
 alpha_token_t* searchToken(alpha_token_t* root, unsigned int num_token){
     alpha_token_t* temp = root;
-    while (temp) {
-        if (temp->num_token == num_token) {
+    while(temp) {
+        if(temp->num_token == num_token) {
             return temp;
         }
         temp = temp->next;
@@ -92,19 +153,35 @@ alpha_token_t* searchToken(alpha_token_t* root, unsigned int num_token){
 }
 
 void printTokens(alpha_token_t* root){
-    //TO DO: to format na einai opws sto FAQ!
-    while (root != NULL) {
-        printf("%u:  #%u  \"%s\"  %s  %s  <-%s\n", root->line, root->num_token, root->zoumi, CAT[root->category], root->subcategory, CAT[root->superclass]);
+    while(root != NULL) {
+        printf("%u:  #%u  \"%s\"  %s  ", root->line, root->num_token, root->zoumi, CAT[root->category]);
+        switch(root->name) {
+            /* INTEGER - REAL */
+            case 45:
+            case 46:
+                printf("%s", root->zoumi);
+                break;
+            /* STRING - ID */
+            case 47:
+            case 48:
+                printf("\"%s\"", root->zoumi);
+                break;
+            default:
+                printf("%s", NAM[root->name]);
+                break;
+        }
+        printf("  <-%s\n", SUP[root->superclass]);
         root = root->next;
     }
 }
 
 void freeTokenList(alpha_token_t** root){
     alpha_token_t* temp;
-    while (*root) {
+    while(*root) {
         temp = *root;
         *root = (*root)->next;
-        free(temp->zoumi); 
-        free(temp);
+        destroyToken(temp);
     }
 }
+
+/* end of list.c */
--- list.h
+++ list.h
@@ -1,3 +1,5 @@
+/* list.h */
+
 #ifndef LIST_H
 #define LIST_H
 
@@ -11,21 +13,25 @@ typedef struct alpha_token_t {
     
     char* zoumi;
     unsigned int category;
-    char* subcategory;
+    unsigned int name;
     unsigned int superclass;
     
     struct alpha_token_t* next; 
 } alpha_token_t;
 
 extern char* CAT[];
+extern char* NAM[];
+extern char* SUP[];
 extern int tokenCounter;
 extern alpha_token_t* root;
 
-alpha_token_t* createTokenNode(unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, char* subcategory, unsigned int superclass);
-void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, char* subcategory, unsigned int superclass);
-void deleteToken(alpha_token_t** root, unsigned int num_token);
+alpha_token_t* createTokenNode(unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, unsigned int name, unsigned int superclass);
 alpha_token_t* searchToken(alpha_token_t* root, unsigned int num_token);
+
+void insertToken(alpha_token_t** root, unsigned int line, unsigned int num_token, char* zoumi, unsigned int category, unsigned int name, unsigned int superclass);
+void deleteToken(alpha_token_t** root, unsigned int num_token);
 void printTokens(alpha_token_t* root);
 void freeTokenList(alpha_token_t** root);
 
-#endif 
+#endif
+/* end of list.h */
