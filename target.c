bad9ba6d9ebd81b424d058b0731e9e9f6ee71959
ElsieUwU <mixaliskritsotakis@gmail.com>
2025-05-28 20:16:06 +0300

    Generators

--- target.c
+++ target.c
@@ -175,167 +175,89 @@ void generate_ASSIGN(quad* q) {
 
 }
 
-void generate_ADD(quad* q) {
-
-} 
-
-void generate_SUB(quad* q) {
-
-} 
-
-void generate_MUL(quad* q) {
-
-}
-
-void generate_DIV(quad* q) {
-
-} 
-
-void generate_MOD(quad* q) {
-
-}
-
+void generate_ADD(quad* q) { helper_generate_full(ADD_V, q); } 
+void generate_SUB(quad* q) { helper_generate_full(SUB_V, q); } 
+void generate_MUL(quad* q) { helper_generate_full(MUL_V, q); }
+void generate_DIV(quad* q) { helper_generate_full(DIV_V, q); } 
+void generate_MOD(quad* q) { helper_generate_full(MOD_V, q); }
 void generate_UMINUS(quad* q) {
-
+    q->arg2 = create_constbool_expr(-1);
+    helper_generate_full(MUL_V, q);
 }
 
-void generate_AND(quad* q) {
+void generate_AND(quad* q) { printf("AND Should not exist\n"); }
+void generate_OR(quad* q) { printf("OR Should not exist\n"); }
+void generate_NOT(quad* q) { printf("NOT Should not exist\n"); }
 
-}
+void generate_IF_EQ(quad* q) { helper_generate_relational(JEQ_V, q); }
+void generate_IF_NOTEQ(quad* q) { helper_generate_relational(JNE_V, q); }
+void generate_IF_LESSEQ(quad* q) { helper_generate_relational(JLE_V, q); }
+void generate_IF_GREATEREQ(quad* q) { helper_generate_relational(JGE_V, q); }
+void generate_IF_LESS(quad* q) { helper_generate_relational(JLT_V, q); }
+void generate_IF_GREATER(quad* q) { helper_generate_relational(JGT_V, q); }
 
-void generate_OR(quad* q) {
+void generate_CALL(quad* q) { helper_generate_arg1(CALL_V, q); }
+void generate_PARAM(quad* q) { helper_generate_arg1(PARAM_V, q); }
 
-}
+void generate_RETURN(quad* q) { helper_generate_res(RETURN_V, q); }
+void generate_GETRETVAL(quad* q) { helper_generate_res(GETRETVAL_V, q); }
 
-void generate_NOT(quad* q) {
+void generate_FUNCSTART(quad* q) { helper_generate_arg1(FUNCSTART_V, q); }
+void generate_FUNCEND(quad* q) { helper_generate_arg1(FUNCEND_V, q); }
 
-}
+void generate_NEWTABLE(quad* q) { helper_generate_res(TABLECREATE_V, q); }
 
-void generate_IF_EQ(quad* q) {
-
-}
-
-void generate_IF_NOTEQ(quad* q) {
-
-}
-
-void generate_IF_LESSEQ(quad* q) {
-
-}
-
-void generate_IF_GREATEREQ(quad* q) {
-
-}
-
-void generate_IF_LESS(quad* q) {
-
-}
-
-void generate_IF_GREATER(quad* q) {
-
-}
-
-void generate_CALL(quad* q) {
-
-}
-
-void generate_PARAM(quad* q) {
-
-}
-
-void generate_RETURN(quad* q) {
-
-}
-
-void generate_GETRETVAL(quad* q) {
-
-}
-
-void generate_FUNCSTART(quad* q) {
-
-}
-
-void generate_FUNCEND(quad* q) {
-
-}
-
-void generate_NEWTABLE(quad* q) {
-
-}
-
-void generate_TABLEGETELEM(quad* q) {
-
-}
-
-void generate_TABLESETELEM(quad* q) {
-
-}
+void generate_TABLEGETELEM(quad* q) { helper_generate_full(TABLEGETELEM_V, q); }
+void generate_TABLESETELEM(quad* q) { helper_generate_full(TABLEGETELEM_V, q); }
 
 void generate_JUMP(quad* q) {
 
 }
 
-void generate_NOP(quad* q) {
-
-}
+void generate_NOP(quad* q) { printf("NOP Should not exist\n"); }
 
 void helper_generate_full(vmopcode op, quad* q){
     instruction* t = (instruction*)malloc(sizeof(instruction));
-    if (!t) { perror("Failed to allocate instruction"); exit(EXIT_FAILURE); }
-
+    if(!t) { MemoryFail(); }
     t->opcode = op;
-
     make_operand(q->arg1, &(t->arg1));
     make_operand(q->arg2, &(t->arg2));
     make_operand(q->result, &(t->result));
-
     q->target_addr = curr_instruction;
     emit_target(t);
 }
 
 void helper_generate_relational(vmopcode op, quad* q){
     instruction* t = (instruction*)malloc(sizeof(instruction));
-    if (!t) { perror("Failed to allocate instruction"); exit(EXIT_FAILURE); }
-
+    if(!t) { MemoryFail(); }
     t->opcode = op;
-
     make_operand(q->arg1, &(t->arg1));
     make_operand(q->arg2, &(t->arg2));
-
     q->target_addr = curr_instruction;
     emit_target(t);
 }
 
 void helper_generate_arg1(vmopcode op, quad* q){
     instruction* t = (instruction*)malloc(sizeof(instruction));
-    if (!t) { perror("Failed to allocate instruction"); exit(EXIT_FAILURE); }
-
+    if(!t) { MemoryFail(); }
     t->opcode = op;
-
     make_operand(q->arg1, &(t->arg1));
     make_operand(NULL, &(t->arg2));
     make_operand(q->result, &(t->result));
-
     q->target_addr = curr_instruction;
     emit_target(t);
 }
 
 void helper_generate_res(vmopcode op, quad* q){
     instruction* t = (instruction*)malloc(sizeof(instruction));
-    if (!t) { perror("Failed to allocate instruction"); exit(EXIT_FAILURE); }
-
+    if(!t) { MemoryFail(); }
     t->opcode = op;
-
     make_operand(NULL, &(t->arg1));
     make_operand(NULL, &(t->arg2)); 
     make_operand(q->result, &(t->result));
-    
-
     q->target_addr = curr_instruction;
     emit_target(t);
 }
-
-
 
 void generate(void) {
     for(unsigned i = 0; i < totalquads; ++i) {
