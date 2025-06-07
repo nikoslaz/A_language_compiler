882675eccb2d08b7a7b275d8823b385acfcd264f
nikoslaz <nikoslazaridis10@gmail.com>
2025-06-07 20:38:52 +0300

    bool relat

--- avm_executors.c
+++ avm_executors.c
@@ -217,5 +217,52 @@ double uminus_arith(double x, double y) { runtimeError("UMINUS SHOULD NOT EXIST"
 
 
 /*===============================================================================================*/
+/* To Bool */
+
+unsigned int number_tobool(memcell* mem) {
+    return mem->data.stackval_zoumi != 0;
+}
+
+unsigned int string_tobool(memcell* mem) {
+    return mem->data.string_zoumi[0] != 0;
+}
+
+unsigned int bool_tobool(memcell* mem) {
+    return mem->data.bool_zoumi;
+}
+
+unsigned int table_tobool(memcell* mem) {
+    return 1;
+}
+
+unsigned int userfunc_tobool(memcell* mem) {
+    return 1;
+}
+
+unsigned int libfunc_tobool(memcell* mem) {
+    return 1;
+}
+
+unsigned int nil_tobool(memcell* mem) {
+    return 0;
+}
+
+unsigned int stackval_tobool(memcell* mem) {
+    return 1;
+}
+
+unsigned int undef_tobool(memcell* mem) {
+    return 0;
+}
+
+/*===============================================================================================*/
+/* Relational */
+
+unsigned int jgt_rel(double x, double y) { return x > y; }
+unsigned int jle_rel(double x, double y) { return x <= y; }
+unsigned int jge_rel(double x, double y) { return x >= y; }
+unsigned int jlt_rel(double x, double y) { return x < y; }
+
+/*===============================================================================================*/
 
 /* end of avm_executors.c */
